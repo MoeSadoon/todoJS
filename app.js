2346474084312ae8ac3a706659c30f96cedfae03
Moe Sadoon <msadoon590@gmail.com>
2017-09-10 17:00:47 +0100

    can now add items to UI

--- app.js
+++ app.js
@@ -2,32 +2,85 @@ const dataController = (function () {
 
     const data = {
         todos: [],
-        done: [],
+        done: []
     }
 
-})();
-
-const uiController = (function () {
-
     return {
-        getInput: function () {
-            const input = document.querySelector('.input').value;
-            console.log(input);
+        addItem: function (input) {
+            data.todos.push(input);
+        },
+
+        test: function () {
+            return data;
         }
     }
 
 })();
 
-const appController = (function (uiCtrl) {
+
+const uiController = (function () {
+
+    const DOMstrings = {
+        todos: '.todos',
+        input: '.input'
+    }
+
+    return {
+        getDOMStrings: function () {
+            return DOMstrings;
+        },
+
+        getInput: function () {
+            let input;
+
+            input = document.querySelector(DOMstrings.input).value;
+            return input;
+        },
+
+        addUI: function (input) {
+            let listItemHTML;
+
+            listItemHTML = document.createElement("li");
+            listItemHTML.appendChild(document.createTextNode(input));
+            document.querySelector(DOMstrings.todos).appendChild(listItemHTML);
+        }
+    }
+
+})();
+
+
+const appController = (function (uiCtrl, dataCtrl) {
+
+    const DOMstrings = uiCtrl.getDOMStrings();
+
+    const setUpEventlistener = () => {
+        document.querySelector(DOMstrings.input).addEventListener('keypress', event => {
+            if (event.keyCode === 13 || event.which === 13) {
+                addItem();
+            }
+        });
+    }
 
     // Add item to controller
     const addItem = () => {
         // Get input from UI
-        uiCtrl.getInput();
+        const input = uiCtrl.getInput();
 
         // Add to data storage
+        dataCtrl.addItem(input);
 
         // Add input to UI
+        uiCtrl.addUI(input);
     }
 
-})(uiController);
+    return {
+        init: function () {
+            console.log("App has started");
+            setUpEventlistener();
+        }
+    }
+
+})(uiController, dataController);
+
+//Initialize app on load
+appController.init();
