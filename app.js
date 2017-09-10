0f4aeb7364f2feffd1432308148d7dccd37e2ba7
Moe Sadoon <msadoon590@gmail.com>
2017-09-10 12:46:37 +0100

    retrieved input from form

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const dataController = (function() {
+const dataController = (function () {
+
+    const data = {
+        todos: [],
+        done: [],
+    }
 
 })();
 
-const uiController = (function() {
+const uiController = (function () {
+
+    return {
+        getInput: function () {
+            const input = document.querySelector('.input').value;
+            console.log(input);
+        }
+    }
 
 })();
 
-const appController = (function() {
+const appController = (function (uiCtrl) {
 
-})();
+    // Add item to controller
+    const addItem = () => {
+        // Get input from UI
+        uiCtrl.getInput();
+
+        // Add to data storage
+
+        // Add input to UI
+    }
+
+})(uiController);
